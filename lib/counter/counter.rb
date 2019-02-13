168c83503895235a28cac350dc8a261d5d89364b
Michael Rosefield <mrosefield@g2g3.digital>
2019-02-13 17:10:51 +0900

    bugfix! multi-line strings now counted correctly

--- lib/counter/counter.rb
+++ lib/counter/counter.rb
@@ -5,7 +5,7 @@ require_relative '..\fs\files'
 
 module Counter
   class CodeLineCounter
-    include Parsing::Lexer, Options, Paths::Files
+    include Parsing::Lexer, Options, FS::Files
 
     attr_reader :result, :options
 
@@ -27,13 +27,13 @@ module Counter
       self
     end
 
-    def set_output(options)
+    def set_output options
       set_options options, OUTPUTS
       self
     end
 
     def count
-      @line_parser = Parsing::LineParser.new(@options)
+      @line_parser = Parsing::LineParser.new @options
       result = @file_paths.map { |file| [file, count_file(file)] }
 
       add_summary result if @options === OUTPUT_SUMMARY
@@ -44,13 +44,13 @@ module Counter
 
     private
 
-    def count_file(file)
+    def count_file file
       get_tokens_for_each_line(file)
         .map { |types| @line_parser.parse types }
         .reduce init_counts, &method(:count_parsed)
     end
 
-    def count_parsed(acc, parsed_line)
+    def count_parsed acc, parsed_line
       parsed_line.each { |count| acc[count] += 1 }
       acc
     end
@@ -59,22 +59,22 @@ module Counter
       counting_options(@options).map { |opt| [opt, 0] }.to_h
     end
 
-    def set_options(options, option_list)
+    def set_options options, option_list
       options_in_list = options & option_list
       @options.merge options_in_list
     end
 
-    def add_summary(result)
+    def add_summary result
       @result[:summary] = result
         .map { |(_, counts)| counts}
         .reduce { |acc, counts| merge_counts acc, counts }
     end
 
-    def merge_counts(acc, counts)
+    def merge_counts acc, counts
       acc.merge(counts) { |type, c1, c2| c1 + c2 }
     end
 
-    def add_file_counts(result)
+    def add_file_counts result
       file_counts = result.to_h
       @result.merge! file_counts
     end
--- lib/parsing/lexer.rb
+++ lib/parsing/lexer.rb
@@ -3,6 +3,7 @@ require 'ripper'
 module Parsing
   COMMENT_TOKENS = %i(comment embdoc_beg embdoc embdoc_end)
   WHITESPACE_TOKENS = %i(nl ignored_nl sp)
+  CUSTOM_ML_STRING_TOKEN = :within_multi_line_string
 
   module Lexer
     def get_tokens_for_each_line(path)
@@ -12,17 +13,27 @@ module Parsing
 
     private
 
-    # Tokenizes file and returns array of tokens for each line
+    # Tokenizes file and returns hash with
+    # - keys: line numbers
+    # - values: array of tokens for that line
     def lex_file(file)
       Ripper
         .lex(file)
-        .reduce({}) do |acc, item|
-          (line_num,), event = item
-          token = event_to_token event
-          (acc[line_num] ||= []) << token
+        .then { |lexed| group_tokens_by_line_num lexed }
+        .then { |grouped| add_missing_lines grouped }
+        .values
+    end
 
-          acc
-        end.map { |_, types| types }
+    private
+
+    def group_tokens_by_line_num token_data
+      token_data.reduce({}) do |acc, item|
+        (line_num,), event = item
+        token = event_to_token event
+        (acc[line_num] ||= []) << token
+
+        acc
+      end
     end
 
     def event_to_token(event)
@@ -30,6 +41,21 @@ module Parsing
         .to_s
         .delete_prefix('on_')
         .to_sym
+    end
+
+    # For some multi-line string-type expressions, Ripper#lex will not return
+    # any information for lines within it, as it's considered part of the
+    # string. This does not occur for multi-line comments, so we can assume
+    # that any missing lines are inside some multi-line string
+    def add_missing_lines tokens_by_line_num
+      keys = tokens_by_line_num.keys
+      max_line = keys.max
+      missing_lines = (2...max_line).reject { |i| keys.include? i }
+      tokens_by_missing_lines = missing_lines
+        .map { |l| [l, [CUSTOM_ML_STRING_TOKEN]] }
+        .to_h
+
+      tokens_by_line_num.merge tokens_by_missing_lines
     end
   end
 end
